5918fd8daac19128f2f56d97d7f2c547b3fbbc85
lancer abir <you@example.com>
2021-08-20 10:39:24 +0600

    commit

--- src/Components/GetCharacters.js
+++ src/Components/GetCharacters.js
@@ -5,60 +5,59 @@ import { Link } from "react-router-dom";
 import { GET_CHARACTERS_QUERY } from "../GraphQL/Queries";
 
 const GetCharacters = () => {
-    const { data } = useQuery(GET_CHARACTERS_QUERY);
+   const { data } = useQuery(GET_CHARACTERS_QUERY);
 
-    const [charactersData, setCharactersData] = useState([]);
+   const [charactersData, setCharactersData] = useState([]);
 
-    useEffect(() => {
-        setCharactersData(data && data.characters.results);
-    }, [data]);
-    console.log(charactersData);
-    return (
-        <div className="pr-5 pl-5">
-            <div className="row">
-                <div className="col-md-12 mt-3 text-center">
-                    <h2>Get Characters Data</h2>
-                </div>
-                {charactersData
-                    ? charactersData.map((data) => (
-                          <div
-                              key={data.id}
-                              className="col-md-3 col-sm-6 col-12 mb-3 item"
-                          >
-                              <div className="card item-card card-block">
-                                  <img
-                                      className="card-img-top image-width"
-                                      src={data.image}
-                                      alt={data.name}
-                                  />
-                                  <div className="pl-3 pb-3 pr-3">
-                                      <h5 className="card-title  mt-3 mb-3">
-                                          {data.name}
-                                      </h5>
-                                      <p className="card-text">
-                                          This is a company that builds
-                                          websites, web apps and e-commerce
-                                          solutions.
-                                      </p>
-                                      <Link to="#" className="btn btn-primary">
-                                          <p
-                                              style={{
-                                                  marginRight: "0px",
-                                                  marginBottom: "-8px",
-                                                  marginTop: "5px",
-                                              }}
-                                          >
-                                              Read More
-                                          </p>
-                                      </Link>
-                                  </div>
-                              </div>
-                          </div>
-                      ))
-                    : "Loading..."}
+   useEffect(() => {
+      setCharactersData(data && data.characters.results);
+   }, [data]);
+   console.log(charactersData);
+   return (
+      <div className="pr-5 pl-5">
+         <div className="row">
+            <div className="col-md-12 mt-3 text-center">
+               <h2>Get Characters Data</h2>
             </div>
-        </div>
-    );
+            {charactersData
+               ? charactersData.map((data) => (
+                    <div
+                       key={data.id}
+                       className="col-md-3 col-sm-6 col-12 mb-3 item"
+                    >
+                       <div className="card item-card card-block">
+                          <img
+                             className="card-img-top image-width"
+                             src={data.image}
+                             alt={data.name}
+                          />
+                          <div className="pl-3 pb-3 pr-3">
+                             <h5 className="card-title  mt-3 mb-3">
+                                {data.name}
+                             </h5>
+                             <p className="card-text">
+                                This is a company that builds websites, web apps
+                                and e-commerce solutions.
+                             </p>
+                             <Link to="#" className="btn btn-primary">
+                                <p
+                                   style={{
+                                      marginRight: "0px",
+                                      marginBottom: "-8px",
+                                      marginTop: "5px",
+                                   }}
+                                >
+                                   Read More
+                                </p>
+                             </Link>
+                          </div>
+                       </div>
+                    </div>
+                 ))
+               : "Loading..."}
+         </div>
+      </div>
+   );
 };
 
 export default GetCharacters;
--- src/Components/GetEpisodes.js
+++ src/Components/GetEpisodes.js
@@ -4,34 +4,44 @@ import React, { useEffect, useState } from "react";
 import { GET_EPISODES_QUERY } from "../GraphQL/Queries";
 
 const GetEpisodes = () => {
-    const { data } = useQuery(GET_EPISODES_QUERY);
+   const { data } = useQuery(GET_EPISODES_QUERY);
 
-    const [episodesData, setEpisodesData] = useState([]);
+   const [episodesData, setEpisodesData] = useState([]);
 
-    useEffect(() => {
-        setEpisodesData(data && data.episodes.results);
-    }, [data]);
-    console.log(episodesData);
-    return (
-        <div className="pr-5 pl-5">
-            <div className="row">
-                <div className="col-md-12 mt-3 mb-3 text-center">
-                    <h2>Get Episodes Data</h2>
-                </div>
+   useEffect(() => {
+      setEpisodesData(data && data.episodes.results);
+   }, [data]);
+   console.log(episodesData);
 
-                {episodesData
-                    ? episodesData.map((data) => (
-                          <div
-                              key={data.id}
-                              className="col-md-3 col-sm-6 col-12 mb-4"
-                          >
-                              Episodes Id: {data.id} Created: {data.created}{" "}
-                          </div>
-                      ))
-                    : "Loading..."}
+   return (
+      <div className="pr-5 pl-5">
+         <div className="row">
+            <div className="col-md-12 mt-3 mb-3 text-center">
+               <h2>Get Episodes Data</h2>
             </div>
-        </div>
-    );
+
+            {episodesData
+               ? episodesData.map((data) => (
+                    <div
+                       key={data.id}
+                       className="col-md-3 col-sm-6 col-12 mb-4"
+                    >
+                       <div
+                          style={{
+                             backgroundColor: `#${Math.floor(
+                                Math.random() * 1677729892
+                             ).toString(16)}`,
+                          }}
+                          className="episodesBox"
+                       >
+                          Episodes Id: {data.id} Created: {data.created}{" "}
+                       </div>
+                    </div>
+                 ))
+               : "Loading..."}
+         </div>
+      </div>
+   );
 };
 
 export default GetEpisodes;
