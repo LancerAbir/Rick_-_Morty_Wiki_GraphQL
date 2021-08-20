eb74560f024406fbb94a8977f7e2f87ff41fffad
lancer abir <you@example.com>
2021-08-20 13:54:17 +0600

    push

--- src/GraphQL/Queries.js
+++ src/GraphQL/Queries.js
@@ -15,7 +15,7 @@ export const GET_EPISODES_QUERY = gql`
 `;
 
 export const GET_SINGLE_EPISODE_QUERY = gql`
-   query EpisodesByIds($id: String) {
+   query EpisodesByIds($id: String!) {
       episodesByIds(ids: $id) {
          id
          episode
@@ -27,6 +27,23 @@ export const GET_SINGLE_EPISODE_QUERY = gql`
 `;
 
 export const GET_CHARACTERS_QUERY = gql`
+   query {
+      characters {
+         results {
+            id
+            name
+            status
+            species
+            type
+            gender
+            image
+            created
+         }
+      }
+   }
+`;
+
+export const GET_SINGLE_CHARACTERS_QUERY = gql`
    query {
       characters {
          results {
